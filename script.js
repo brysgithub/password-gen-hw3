5b80d328a9d255c5ceacb1725a572c6ce237f5c6
goblin.brn <bschenck0@gmail.com>
2021-11-19 22:14:12 -0800

    Added mergeArray funcion to create new array to pull chcaracters to password and make sure each bool true is included in password

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ var boolSym;
 var boolInt;
 var boolEmo;
 
+var trueAnswers = [boolUpc, boolLow, boolSym, boolInt, boolEmo];
+
+
 // Character Library
 var upc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var low = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -16,8 +19,39 @@ var int = ["1","2","3","4","5","6","7","8","9","0"];
 var sym = ["!","@","#","%","^","&","*","(",")","-","_","=","+","?","<",">","[","]","{","}","|","/"];
 var emo = ["🤡","🦍","🧠","👻","👽","☠️","🗿","💯","🅱️"];
 
+var charUsed = [];
+var passString = "";
+
+// make one big array
+function mergeArray() {
+  if (boolUpc) {
+    charUsed = upc.concat(charUsed);
+    passString += randomChar(upc);
+  }
+
+  if (boolLow) {
+    charUsed = low.concat(charUsed);
+    passString += randomChar(low);
+  }
+
+  if (boolInt) {
+    charUsed = int.concat(charUsed);
+    passString += randomChar(int);
+  }
+
+  if (boolSym) {
+    charUsed = sym.concat(charUsed); 
+    passString += randomChar(sym);
+  }
+
+  if (boolEmo) {
+    charUsed = emo.concat(charUsed); 
+    passString += randomChar(emo);
+  }
+}
+
 // Gather user info
-function userOptions() {
+function generatePassword() {
   alert("Choose password settings");
   passLength = parseInt(prompt("Choose password length (8 to 128 characters)")); 
   alert("Password length set to " + passLength)
@@ -27,7 +61,7 @@ function userOptions() {
     } else if (isNaN(passLength) === true){
       alert("Please enter a number. Max 128 Min 8")
       return;
-    } else alert("Password length OK")
+    } else alert("Password length OK");
   boolUpc = confirm("Include UPPERCASE letters?");
   boolLow = confirm("Include lowercase letters?");
   boolInt = confirm("Include numbers?");
@@ -36,20 +70,13 @@ function userOptions() {
     if (boolUpc === false && boolLow === false && boolInt === false && boolSym === false && boolEmo === false) {
       alert("Please choose at least one option. Help me out here.");
       return;
-    };
-    
-    // User options object
-    var passOptions = {
-      passLength: passLength,
-      boolUpc: boolUpc,
-      boolLow: boolLow,
-      boolInt: boolInt,
-      boolSym: boolSym,
-      boolEmo: boolEmo
-    };
-    return passOptions;
-    
-  } 
+    } else mergeArray(); randomChar(charUsed); // loopFillPass(charUsed);
+
+    return passString;
+};
+
+console.log(passString)
+console.log(mergeArray)
 
 // random character from array
 function randomChar(array) {
@@ -58,49 +85,16 @@ function randomChar(array) {
   return randomSelection;
 };
 
-// choose characters from user input keys in passOptions and push into array
-function generatePassword() {
-  var userInput = userOptions();
-  var passMem = [];
-  var charBanked = [];
-  var charSelected = [];
-  if (userInput.boolUpc) {
-    charSelected = charBanked.concat(upc);
-    charSelected.push(randomChar(passMem));
-  }
+// loop times minus number of true user choice booleans
+function loopFillPass(array) { 
+  var addTrue = trueAnswers.filter(Boolean).length;
+  console.log(addTrue)
+  // for (let i = 0 + addTrue; i < array.length; i++) {
+  // const element = array[i];
   
-  if (userInput.boolLow) {
-    charSelected = charBanked.concat(low);
-    charSelected.push(randomChar(passMem));
-  }
-  
-  if (userInput.boolInt) {
-    charSelected = charBanked.concat(int);
-    charSelected.push(randomChar(passMem));
-  }
-  
-  if (userInput.boolSym) {
-    charSelected = charBanked.concat(sym);
-    charSelected.push(randomChar(passMem));
-  }
-  
-  if (userInput.boolEmo) {
-    charSelected = charBanked.concat(emo);
-    charSelected.push(randomChar(passMem));
-  }
-
-  for (let index = 0; index < userInput.length; index++) {
-    const charsBanked = randomChar(charBanked);
-    passMem.push(charsBanked);
-  } 
-
-  for (let index = 0; index < charSelected.length; index++) {
-    passMem[index] = charSelected[index];
-  }
-
-  return passMem.join("");
-};
-
+  //}
+}
+// writes password to form
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
